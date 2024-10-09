65e966664239deb8b124ebfe637ee9d6c7fe6ccf
brianmunenee <brianmunene005@gmail.com>
2024-10-09 21:46:15 +0300

    add file

--- while loop.cpp
+++ while loop.cpp
@@ -1,16 +1,15 @@
-#include <stdio.h>
-int main () {
-	int i =0, start ,stop,sum;
+# include <stdio.h>
+int main (){
+	int i =0,start,stop,sum =0;
 	printf("enter the value to start:");
 	scanf("%d",&start);
-	printf("enter the value when to stop:");
+	printf("enter the value to stop:");
 	scanf("%d",&stop);
-	while (i<=100){
+	while (i<=100) {
 		printf("%d\n",i);
 		i++;
-		sum +=i;
-		}
+		sum +=i;}
 		printf("the sum is:%d",sum);
-	return 0;
+		return 0;
 }
-		
+	
